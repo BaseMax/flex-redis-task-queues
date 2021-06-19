fcf04dbe88fe404c0d5cd09aec1272d4211439c8
Max Base <MaxBaseCode@gmail.com>
2021-06-20 04:23:41 +0430

    Rename app.py to run.py, so this will runned by flask run

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ r = redis.Redis()
 q = Queue(connection=r)
 
 def background_task(n):
+	""" Function that simulates a delay """
 	delay = 2
 	print("Task running")
 	print(f"Simulating {delay} second delay")
@@ -16,12 +17,16 @@ def background_task(n):
 	print("Task complete")
 	return len(n)
 
-@app.route("/task")
+@app.route("/task", methods=["GET", "POST"])
 def add_task():
+	global q
 	if request.args.get("n"):
+		n = request.args.get("n")
+		print(f"Your n is {n}")
+		job = q.enqueue(background_task, n)
 		job = q.enqueue(background_task, request.args.get("n"))
 		q_len = len(q)
-		return f"Task {job.id} added to queue at {job.enquenued_at}. {q_len} tasks in the queue"
+		return f"Task {job.id} added to queue at {job.enqueued_at}. {q_len} tasks in the queue"
 	return "N value for n"
 
 if __name__ == "__main__":
